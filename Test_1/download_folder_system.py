6a395d8240b601ec7a88f08d1bb2eea2093e7c90
Floorey <154369071+Floorey@users.noreply.github.com>
2024-02-02 09:08:21 +0100

    some list tests

---
+++ Test_1/download_folder_system.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime, timedelta
+
+def delete_old_files(folder_path, older_than_days=14):
+    now = datetime.now()
+    older_than = now  -timedelta(days=older_than_days)
+
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        modified_at = datetime.fromtimestamp(os.path.getmtime(file_path))
+
+
+        if modified_at < older_than:
+            os.remove(file_path)
+            print(f'{file} has been deleted')
+            
+    
+
+folder_path = "C:/Users/lukas/Downloads"
+delete_old_files(folder_path)
+
+
+  
