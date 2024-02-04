50e0e0684ab6d23d5f9c8bc04bb4181cd9af8e90
Floorey <154369071+Floorey@users.noreply.github.com>
2024-02-04 11:34:38 +0100

    added max Value to analyse class

--- Test_1/listmerge_test.py
+++ Test_1/listmerge_test.py
@@ -1,12 +1,24 @@
 import numpy as np
 
-def analyse_values():
-    max_value1 = np.max(array_1)
-    max_value2 = np.max(array_2)
-    return max_value1, max_value2
-    
+
+
+
+class AnalyseValues():
+    def __init__(self):
+        self.max_value1 = None
+        self.max_value2 = None
+
+
+    @staticmethod
+    def analyse_values_max(array_1, array_2):
+        max_value1 = np.max(array_1)
+        max_value2 = np.max(array_2)
+        return max_value1, max_value2
     
 
+
+   
+   
 
 array_1 = np.random.rand(10)
 array_2 = np.random.rand(50)
@@ -14,9 +26,17 @@ print(f"Generated values: {array_1}")
 print(f"Generated values: {array_2}")
 
 
-result_1 = analyse_values(array_1)
-result_2 = analyse_values(array_2)
+
+
+analyse_instance = AnalyseValues()
+result_1, result_2 = AnalyseValues.analyse_values_max(array_1, array_2)
+
+analyse_instance = result_1
+analyse_instance = result_2
+
 print(f"Max value 1 is: {result_1}")
-print(f'Max value 2 is: {result_2}')
+print(f"Max value 2 is: {result_2}")
+
+
 
 
