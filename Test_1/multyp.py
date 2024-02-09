00a73692e4612a1d824839e8384c2d5568d5eb23
Floorey <154369071+Floorey@users.noreply.github.com>
2024-02-09 18:50:35 +0100

    added DBSCAN as nwe clustering methode

--- Test_1/multyp.py
+++ Test_1/multyp.py
@@ -3,8 +3,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.metrics.pairwise import pairwise_distances_argmin
+import time
 
-image_path = image_path = r"C:\Users\lukas\OneDrive\Desktop\Bilder\zoey-deutch-65343-1653908496-w-893.jpg.jpg"
+image_path = image_path = r"C:/Users/lukas/OneDrive/Desktop/Bilder/1134871.jpg"
 image = Image.open(image_path)
 
 image_array = np.array(image)
@@ -15,17 +16,20 @@ X = image_array.reshape((height*width, channels))
 n_clusters = 5
 
 #KMeans clustering
+start_time = time.time()
 k_means = KMeans(n_clusters=n_clusters)
 k_means.fit(X)
 k_means_labels = k_means.predict(X)
 k_means_cluster_centers = k_means.cluster_centers_
-
+k_means_time = time.time() - start_time
 
 #MiniBatchKmeans-Clusrering
+start_time = time.time()
 mbk = MiniBatchKMeans(n_clusters=n_clusters)
 mbk.fit(X)
 mbk_labels = mbk.predict(X)
 mbk_cluster_centers = mbk.cluster_centers_
+mbk_time = time.time() - start_time
 
 
 order = pairwise_distances_argmin(k_means_cluster_centers, mbk_cluster_centers)
@@ -45,3 +49,21 @@ axes[2].imshow(mbk_cluster_centers[order[mbk_labels]].reshape(height, width, cha
 axes[2].set_title("MiniBatchKMeans Clustered")
 
 plt.show()
+
+
+print('KMeans Cluster Centers')
+print(k_means_cluster_centers)
+
+print('MiniBatchKMeans Cluster Centers:')
+print(mbk_cluster_centers[order[mbk_labels]])
+
+k_means_colors = k_means_cluster_centers.astype(np.uint8)
+mbk_colors = mbk_cluster_centers[order[mbk_labels]].astype(np.uint8)
+
+print('KMEans Cluster Colors:')
+print(k_means_colors)
+print('MiniBatchKMEans Cluster Colors:')
+print(mbk_colors)
+
+print("Zeit für KMeans-Clustering:", k_means_time, "sec")
+print("Zeit für MiniBatchKMeans-Clustering:", mbk_time, "secn")
